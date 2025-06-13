de100f1424ed95d8b0b44c51f74ad070e54c6ea2
deloabass <delomodibo@gmail.com>
2025-06-13 22:59:06 +0100

    b

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import TabLayout from "./app/(tabs)/_layout";
 import SplashScreen from "./app/SplashScreen";
